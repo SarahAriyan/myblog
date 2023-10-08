c33ca89989a5c8529186c962e42f099164485a6f
SarahAriyan <sarah.aryan1380@gmail.com>
2023-10-08 17:50:07 +0330

    f4

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('create100/',views.create100), 
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 def create100(requests):
     me = User.objects.get(username='admin')
@@ -14,5 +15,9 @@ def post_list(request):
     ls= Post.objects.all()
     content={"posts":ls}
     return render(request, 'blog/post_list.html',content)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
